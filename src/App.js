1ec0a05e3c27a9957def0770fc617168087fd27f
thonino <thoninoben@gmail.com>
2023-12-01 13:45:32 +0100

    Create : CrudCars2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 // import Compteur from "./components/Compteur/Compteur";
 // import TraitementCar from './components/CrudCarsExport/TraitementCar/TraitementCar';
-import CrudCars from './components/CrudCars/CrudCars';
+// import CrudCars from './components/CrudCars/CrudCars';
+import CrudCars2 from './components/CrudCars/CrudCars2';
 
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <div className="App">
       {/* <Compteur /> */}
       {/* <TraitementCar /> */}
-      <CrudCars />
+      {/* <CrudCars /> */}
+      <CrudCars2 />
     </div>
   );
 }
