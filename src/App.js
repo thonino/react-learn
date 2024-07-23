936a74d8e1e68498ca1c235ff2bca3b172c2efd4
thonino <thoninoben@gmail.com>
2024-07-23 21:14:49 +0200

    reset

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Bienvenue sur mon site</h1>
+        <main>
+        <p>Ceci est un exemple de projet utilisant uniquement React.</p>
+        </main>
       </header>
     </div>
   );
