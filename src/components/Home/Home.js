b53bf1c5bcf5fa018671dea001c4553edb187a73
thonino <thoninoben@gmail.com>
2024-07-29 01:00:54 +0200

    daily

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,67 +1,82 @@
-import React, { useState } from 'react'
+import React, { useState, useRef} from 'react'
 
 function Home() {
-  // Tab
-  const [ tabs, setTabs ] = useState([
-    { id : 0, nom : "item-1" }, 
-    { id : 1, nom : "item-2" }, 
-    { id : 2, nom : "item-3" }
-  ]);
 
-  // Create
-  const handleSubmit = (e) => {
-    const id = tabs.length;
-    const nom = e.target.newItem.value;
-    const newData = { id, nom};
-    nom && setTabs([...tabs, newData])
+  // Show :
+  const [tab, setTab] = useState([
+    { id: 0, name: 'Salon'},
+    { id: 1, name: 'Cuisine'},
+    { id: 2, name: 'Chambre'},
+  ])
+
+  // Ref for the new input field
+  const resetInput = useRef(null);
+
+  // Add :
+  const handleSubmit = (e)=>{
     e.preventDefault();
+    const id = tab.length;
+    const name = e.target.new.value;
+    setTab([...tab, { id , name}])
+  
+    // Reset the input field
+  if (resetInput.current) { resetInput.current.value = '';}
   }
 
-  // Edit
-  const [updateItem,  setUpdateItem] = useState();
+  // Update :
+  const [update, setUpdate] = useState();
   const handleUpdate = (data) => {
-    const newTabs = tabs.map((item) => 
-      item.id === data.id ? { ...item, nom: updateItem} : item
+    const newTab = tab.map((item)=>
+      item.id === data.id ? {...item, name: update} : item
     );
-    setTabs(newTabs);
+    setTab(newTab);
   }
 
+  // Delete :
+  const handleDelete = (data) => {
+    const newTab = tab.filter((item) => item.id !== data.id);
+    setTab(newTab);
+  }
   return (
-    <div>
-      <header className="element ">
-        <h1>Crud</h1>
-      { 
-        tabs.map((data) => 
-        <div key={data.id}> {data.nom} 
-          <div>
-            <input  
-              className='input-form'
-              defaultValue={ updateItem ? updateItem : data.nom}
-              onChange={(e) => setUpdateItem(e.target.value)} 
-            />
-            <button 
-              className='btn'
-              onClick={() => handleUpdate(data)}
-            >  
-              Apply 
-            </button>
-          </div>
-        </div>
-        ) 
-      }
-      <form onSubmit={handleSubmit} className='center'>
-        <input type="text" name="newItem" className='input-form'/>
-        <button type="submit" className="btn"> 
-          Add
-        </button>
-        
+    <div className='center'>
+      <h1>Liste pieces</h1>
+        {tab.map((data)=>
+      <div key={data.id} className='mt'> 
+        {data.name} 
+          <input 
+            className='input-form ms' 
+            defaultValue={update ? update : data.name}
+            onChange={(e)=>setUpdate(e.target.value)}
+          />
+          <button className='btn' onClick={()=>handleUpdate(data)}>
+            Apply
+          </button>
+          <button className='btn-red ms' onClick={()=>handleDelete(data)}> 
+            Remove
+          </button>
+      </div>
+      )}
+      <form onSubmit={handleSubmit}>
+        <h1> Ajouter Piece</h1>
+        <input 
+          type="text" 
+          className='input-form' 
+          name="new" 
+          ref={resetInput}
+        />
+        <button type="submit" className='btn'> Add</button>
       </form>
-      </header>
     </div>
   )
 }
 
 export default Home
 
+// Ref for the new input field
+// const resetInput = useRef(null);
 
-  
+// Reset the input field
+//   if (resetInput.current) { resetInput.current.value = '';}
+//   }
+
+// ref={resetInput}
