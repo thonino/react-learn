971ab535791d21b3fa82fa9ce5769e1b85098ed8
thonino <thoninoben@gmail.com>
2024-08-07 23:13:19 +0200

    test

--- src/components/Hook/Hook-useState.js
+++ src/components/Hook/Hook-useState.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-// Permet de gérer des données qui changent dans un composant, 
-// comme le contenu d'un formulaire ou l'état d'un composant. 
-// Exemple : Gestion d'un compteur dans un composant.
+// // Permet de gérer des données qui changent dans un composant, 
+// // comme le contenu d'un formulaire ou l'état d'un composant. 
+// // Exemple : Gestion d'un compteur dans un composant.
 
-function Counter() {
-  const [count, setCount] = useState(0);
+// function Counter() {
+//   const [count, setCount] = useState(0);
 
-  return (
-    <div>
-      <p>Compteur : {count}</p>
-      <button onClick={
-        () => setCount(count + 1)
-        }
-      >
-        Incrémenter
-      </button>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <p>Compteur : {count}</p>
+//       <button onClick={
+//         () => setCount(count + 1)
+//         }
+//       >
+//         Incrémenter
+//       </button>
+//     </div>
+//   );
+// }
 
 
-export default Counter;
+// export default Counter;
