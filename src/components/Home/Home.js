e48173c708e7782099e91738a2244d4269dec53e
thonino <thoninoben@gmail.com>
2024-08-15 10:00:00 +0000

    exercice crud blind tes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,77 +1,79 @@
 import React, { useState, useRef } from 'react'
 
 function Home() {
-  // Show :
+  // Show
   const [tabs, setTabs] = useState([
-    {id : 0 , name : 'Geographie'},
-    {id : 1 , name : 'Histoire'},
-    {id : 2 , name : 'Mathematique'},
+    {id : 0, name : 'Geo'},
+    {id : 1, name : 'Maths'},
+    {id : 2, name : 'Eco'},
   ])
 
-  // Reset input 1 :
+  // Reset Input
   const resetInput = useRef(null);
-  
-  // Add :
-  const handleSubmit = (e) => {
+
+  // Add
+  const handleAdd = (e) => {
     e.preventDefault();
-    const id =  tabs.strength;
+    const id = tabs.strength;
     const name = e.target.new.value;
-    setTabs([...tabs, { id , name}])
-    if(resetInput.current){
-      resetInput.current.value = ""
-    }
+    setTabs([...tabs , { id, name}]);
+    if(resetInput.current){resetInput.current.value = ''}
   }
 
-  // Update :
+  // Update
   const handleUpdate = (e, id) => {
     e.preventDefault();
     const name = e.target.update.value;
     const newTabs = tabs.map((item) => 
-      item.id === id ? { ...item, name} : item
+      item.id === id ? { id, name } : item
     )
     setTabs(newTabs);
   }
-  // Delete :
+  
+  // Delete
   const handleDelete = (id) => {
-    const newTabs = tabs.filter((item) => item.id !== id);
-    setTabs(newTabs)
+    const newTabs = tabs.filter(item => 
+      item.id !== id
+    )
+    setTabs(newTabs);
   }
 
   return (
-    <div className="center">
-      <h1> List </h1>
-      {tabs.map((data) => 
-      <div key={data.id} className='mt'>
-        <h3>{data.name}</h3>
-        <form 
-          onSubmit={(e)=>handleUpdate(e, data.id)}
-        >
-          <input 
-            defaultValue={data.name}
-            name='update'
-            className="input-form"
-          />
-          <button type="submit" className="btn">
-            Apply
-          </button>
-          <button 
-            onClick={()=>handleDelete(data.id)} 
-            className="btn-red ms"
-          >
-            Remove
-          </button>
-        </form>
-      </div>
+    <div className='center'>
+      <h1> List :</h1>
+      {tabs.map(data => 
+        <div key={data.id}>
+          <h3>{data.name}</h3>
+          <form onSubmit={(e)=>handleUpdate(e, data.id)}>
+            <input 
+              name="update"
+              className='input-form'
+              defaultValue={data.name}
+            />
+            <button type='submit' className='btn'>
+              Apply
+            </button>
+            <button 
+              onClick={()=>handleDelete(data.id)} 
+              className='btn-red'
+            >
+              Delete
+            </button>
+          </form>
+        </div>
       )}
-      <form onSubmit={handleSubmit}>
-        <h1> Add</h1>
+      <form onSubmit={handleAdd}>
+        <h1> Add :</h1>
         <input 
-          type="text" 
-          className="input-form"
           name="new"
+          className='input-form'
+          type='text'
           ref={resetInput}
         />
-        <button type="submit" className="btn">
+        <button 
+          type='submit'
+          className='btn'
+        >
           Add
         </button>
       </form>
